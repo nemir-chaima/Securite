1e8bf8f9a0b27943c773d791079d8048ae2d603b
Chaima Nemir <100592889+nemir-chaima@users.noreply.github.com>
2023-02-10 18:13:30 +0100

    Add files via upload

--- securite/registration.sql
+++ securite/registration.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 09 jan. 2020 à 12:07
--- Version du serveur :  5.7.19
--- Version de PHP :  7.1.9
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,10 +6,7 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
 
 --
 -- Base de données :  `registration`
